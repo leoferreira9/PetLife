0c27f05b0b644944027249f4f8d4e36ec48ba2c0
Leonardo Ferreira <leobarros_@outlook.com>
2023-11-27 13:48:37 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let elementos = document.querySelectorAll('.duvida')
+elementos.forEach(function(duvida){
+    duvida.addEventListener('click', function(){
+        duvida.classList.toggle('ativa')
+    })
+})
+
